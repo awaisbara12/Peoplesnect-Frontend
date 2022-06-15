ba3c15dccc0777af4c81dbdd0074b463a3269909
ranashamshair <ranashamshair12345@gmail.com>
2022-06-15 14:23:43 +0500

    set layout container in newsfeed

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,7 @@ export default function Home() {
         <meta name="description" content="Connect peoples proffasoinaly" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<<<<<<< HEAD
-      <NewsFeed />
-=======
       <ProfileCard />
->>>>>>> c727d6515b4e7d905638c415420731d1c8bb0003
     </RootLayout>
   );
 }
