9fc5e46f9912409ab47527e5221f87de9aba9820
ranashamshair <ranashamshair12345@gmail.com>
2022-06-17 21:46:42 +0500

    component error

--- pages/config.js
+++ pages/config.js
@@ -1,4 +1,12 @@
+import React from "react";
+
 export const SIGN_UP_API_KEY =
   "https://peoplesnect-backend.herokuapp.com/users";
 export const SIGN_IN_API_KEY = process.env.SIGN_IN_API_URL;
 export const SIGN_OUT_API_KEY = process.env.SIGN_OUT_API_URL;
+
+const config = () => {
+  return <div></div>;
+};
+
+export default config;
