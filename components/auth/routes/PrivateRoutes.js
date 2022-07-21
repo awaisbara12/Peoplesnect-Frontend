7d9709770ffabc92d5fa16abac732a3ac3601631
ranashamshair <ranashamshair12345@gmail.com>
2022-07-21 14:29:16 +0500

    error test

--- components/auth/routes/PrivateRoutes.js
+++ components/auth/routes/PrivateRoutes.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Spinner from "../../common/Spinner";
+import { fetchUser } from "../../../store/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const PrivateRoutes = ({ children }) => {
   const router = useRouter();
   const [loader, setLoader] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  var { data: user } = useSelector((state) => state.user);
 
   if (typeof window !== "undefined") {
     var authKey = window.localStorage.getItem("keyStore");
@@ -13,6 +22,10 @@ const PrivateRoutes = ({ children }) => {
   useEffect(() => {
     if (!authKey) {
       router.push("/login");
+    } else {
+      if(user.error){
+        router.push('/login')
+      }
     }
   }, []);
 
