ce5d4ac04e38d387db1032adfd3a2098241a468c
ranaahmadraza1155 <@#ahmadraza1155@#>
2022-12-13 20:08:38 +0500

    Group create & show

--- components/group/Groups.js
+++ components/group/Groups.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import ProfileAvatar from "../../public/images/profile-avatar.png";
 import Cover from "../../public/images/main-banner.jpg";
 import { XCircleIcon } from "@heroicons/react/solid";
+import { GROUP_API } from "../../pages/config";
 
 const Groups = () => {
+  const [joingroups,setJoinGroups] = useState();
+  // Bareer Key
+  if (typeof window !== "undefined") {var authKey = window.localStorage.getItem("keyStore"); }
+  console.log(joingroups)
+  
+  // suggested Group
+  const SuggestedGroups =()=>{
+      const res = fetch(GROUP_API, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: `${authKey}`,
+      },
+      })
+      .then((resp) => resp.json())
+      .then((result) => {
+        setJoinGroups(result.data)
+      })
+  }
+  useEffect(() => {
+    SuggestedGroups();
+  },[])
   return (
     <div className="mt-8">
     <div className="w-[620px] xl:w-[980px] lg:w-[730px] md:w-[780px] px-5 md:px-0 lg:px-0">
@@ -244,7 +267,101 @@ const Groups = () => {
               </div>
             </div>
             <div className="grid grid-cols-1 gap-6 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2">
-              <div className="profile mt-10 border rounded-xl">
+              {joingroups?(
+                joingroups.map((i)=>(
+                  <div className="profile mt-10 border rounded-xl" key={i.id}>
+                    <div className="relative cover">      
+                      {/* Cover Photo */}
+                      {i.cover_image_url?(
+                       <img
+                        src={i.cover_image_url}
+                        className="object-cover rounded-t-xl h-[96px] w-[620px]"
+                        alt=""
+                       />
+                      ):(
+                        <Image
+                          className="object-cover rounded-t-xl"
+                          src={Cover}
+                          width={620}
+                          height={200}
+                          alt=""
+                        />
+                      )}   
+                      {/* Dp */}
+                      {i.display_image_url?(
+                         <div className="absolute -bottom-8 left-2">
+                          <img
+                            src={i.display_image_url}
+                            className="object-cover rounded-full z-40 h-[70px] w-[70px]"
+                              alt=""
+                          />
+                        </div>
+                      ):(
+                        <div className="absolute -bottom-8 left-2">
+                        <Image
+                          className="object-cover"
+                          src={ProfileAvatar}
+                          width={55}
+                          height={55}
+                          alt=""
+                        />
+                        </div>
+                      )}
+                      <div className="absolute top-2 right-1">
+                        <Link href="./">
+                          <a>
+                            <XCircleIcon className="w-5 h-5 hover:w-10 hover:h-10 transition-all text-white" />
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="Details px-4 ">
+                      <a href="group-page/suggest-group">
+                        <div className="ml-16">
+                          <div className="User-Name font-bold capitalize">{i.title.substring(0,25)}</div>
+                        </div>
+                        <div className="details mt-5 font-light">
+                         {i.description.substring(0,30)}
+                        </div>
+                      </a>
+                      {i.group_members_count?(
+                        <div className="followers mt-2 font-extralight">
+                          {i.group_members_count} Members
+                        </div>
+                      ):(
+                        <div className="followers mt-2 font-extralight">
+                          0 Members
+                        </div>
+                      )}                     
+                      {i.group_type=="private_group"?(
+                        <div className="Group-type float-right my-2">
+                          Group:{" "}
+                          <a href="">
+                            <span className="text-indigo-400 font-bold">Private</span>
+                          </a>
+                        </div>
+                      ):('')}
+                      {i.group_type=="public_group"?(
+                        <div className="Group-type float-right my-2">
+                          Group:{" "}
+                          <a href="">
+                            <span className="text-indigo-400 font-bold">Public</span>
+                          </a>
+                        </div>
+                      ):('')}
+                      {/* <a href="group-page/suggest-group"> */}
+                      <button 
+                        className="w-full bg-indigo-400 text-white rounded-xl py-2 hover:text-indigo-400 hover:bg-transparent  border-1 border-indigo-400 mt-2 mb-4"
+                        onClick={()=>alert(i.id)}>
+                          Join Group
+                      </button>
+                      {/* </a> */}
+                    </div>
+                  </div>
+                ))
+              ):('')}
+              
+              {/* <div className="profile mt-10 border rounded-xl">
                 <div className="relative cover">
                   <Link href="group-page/suggest-group">
                     <a>
@@ -361,66 +478,7 @@ const Groups = () => {
                     </button>
                   </a>
                 </div>
-              </div>
-              <div className="profile mt-10 border rounded-xl">
-                <div className="relative cover">
-                  <Link href="group-page/suggest-group">
-                    <a>
-                      <Image
-                        className="object-cover rounded-t-xl"
-                        src={Cover}
-                        width={620}
-                        height={200}
-                        alt=""
-                      />
-                    </a>
-                  </Link>
-                  <div className="absolute -bottom-8 left-2">
-                    <Link href="group-page/suggest-group">
-                      <a>
-                        <Image
-                          className="object-cover"
-                          src={ProfileAvatar}
-                          width={55}
-                          height={55}
-                          alt=""
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                  <div className="absolute top-2 right-1">
-                    <Link href="./">
-                      <a>
-                        <XCircleIcon className="w-5 h-5 hover:w-10 hover:h-10 transition-all text-white" />
-                      </a>
-                    </Link>
-                  </div>
-                </div>
-                <div className="Details px-4 ">
-                  <a href="group-page/suggest-group">
-                    <div className="ml-16">
-                      <div className="User-Name font-bold ">Group Name</div>
-                    </div>
-                    <div className="details mt-5 font-light">
-                      Group Discription And All Details About Admins
-                    </div>
-                  </a>
-                  <div className="followers mt-2 font-extralight">
-                    25,964 Members
-                  </div>
-                  <div className="Group-type float-right my-2">
-                    Group:{" "}
-                    <a href="">
-                      <span className="text-indigo-400 font-bold">Private</span>
-                    </a>
-                  </div>
-                  <a href="group-page/suggest-group">
-                    <button className="w-full bg-indigo-400 text-white rounded-xl py-2 hover:text-indigo-400 hover:bg-transparent  border-1 border-indigo-400 mt-2 mb-4">
-                      Join Group
-                    </button>
-                  </a>
-                </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
